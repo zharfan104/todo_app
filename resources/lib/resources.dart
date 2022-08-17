f5acf6fed3a6f633fe009ccd3cacd14844366e75
zharfan104 <zharfan.andriawan@sejutacita.id>
2022-08-17 16:37:47 +0700

    Add resources main index file

--- resources/lib/resources.dart
+++ resources/lib/resources.dart
@@ -1,7 +1,2 @@
-library resources;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'constant/index.dart';
+export 'models/index.dart';

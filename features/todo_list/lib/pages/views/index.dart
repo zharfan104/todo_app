87c3b07e05a55117da9da9b19e86d997b2c907d4
zharfan104 <zharfan.andriawan@sejutacita.id>
2022-08-21 17:34:06 +0700

    Add TodoListAppBarView

--- features/todo_list/lib/pages/views/index.dart
+++ features/todo_list/lib/pages/views/index.dart
@@ -1,1 +1,2 @@
+export 'todo_list_app_bar_view.dart';
 export 'todo_list_body_view.dart';
---
+++ features/todo_list/lib/pages/views/todo_list_app_bar_view.dart
@@ -0,0 +1,28 @@
+import 'package:component/component.dart';
+import 'package:dependencies/dependencies.dart';
+import 'package:flutter/material.dart';
+
+import '../../cubits/index.dart';
+
+class TodoListAppBarView extends StatelessWidget
+    implements PreferredSizeWidget {
+  const TodoListAppBarView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MyAppBar(
+      title: 'To do list',
+      actions: [
+        IconButton(
+          onPressed: context.read<LoadTasksCubit>().sortAllTasks,
+          icon: const Icon(Icons.refresh_outlined),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => MyAppBar.myAppBarSize;
+}

53c270bb1bd292682a4df2ab1673f7e8501fac67
zharfan104 <zharfan.andriawan@sejutacita.id>
2022-08-21 13:01:45 +0700

    Add TodoListItemWidget

---
+++ features/todo_list/lib/pages/views/widgets/index.dart
@@ -0,0 +1,1 @@
+export 'todo_list_item_widget.dart';
---
+++ features/todo_list/lib/pages/views/widgets/todo_list_item_widget.dart
@@ -0,0 +1,28 @@
+import 'package:dependencies/dependencies.dart';
+import 'package:flutter/material.dart';
+import 'package:resources/resources.dart';
+
+import '../../../cubits/index.dart';
+
+class TodoListItemWidget extends StatelessWidget {
+  const TodoListItemWidget({
+    Key? key,
+    required this.index,
+    required this.task,
+  }) : super(key: key);
+
+  final int index;
+  final TaskMdl task;
+
+  @override
+  Widget build(BuildContext context) {
+    return CheckboxListTile(
+      title: Text(task.description ?? ''),
+      autofocus: false,
+      activeColor: Colors.green,
+      checkColor: Colors.white,
+      value: task.completed ?? false,
+      onChanged: (_) => context.read<LoadTasksCubit>().updateTasks(index),
+    );
+  }
+}

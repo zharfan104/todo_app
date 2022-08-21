ed087faecdaec28933f6c03bda6d08483783d90e
zharfan104 <zharfan.andriawan@sejutacita.id>
2022-08-21 17:34:46 +0700

    Add AddTaskFABWidget

---
+++ features/todo_list/lib/pages/widgets/add_task_fab_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AddTaskFABWidget extends StatelessWidget {
+  const AddTaskFABWidget({
+    Key? key,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: onPressed,
+      focusElevation: 5,
+      child: const Icon(Icons.edit, color: Colors.white),
+    );
+  }
+}

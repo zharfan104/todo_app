626b80a6585cc008d1949d9f51560468e2cf2c47
zharfan104 <zharfan.andriawan@sejutacita.id>
2022-08-21 17:12:48 +0700

    Add actions to MyAppBar

--- shared_libraries/component/lib/app_bars/my_app_bar.dart
+++ shared_libraries/component/lib/app_bars/my_app_bar.dart
@@ -4,15 +4,18 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MyAppBar({
     Key? key,
     required this.title,
+    this.actions,
   }) : super(key: key);
 
   final String title;
+  final List<Widget>? actions;
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       centerTitle: true,
       title: Text(title),
+      actions: actions,
     );
   }
 

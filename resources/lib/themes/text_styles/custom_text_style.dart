1d3ae3b61ea2e449c0b10e84e5cca8af40e6f241
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-09-17 17:03:55 +0700

    Add CustomTextStyles class

---
+++ resources/lib/themes/text_styles/custom_text_style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomTextStyles {
+  static const bodyText = TextStyle(
+    fontSize: 16.00,
+    fontWeight: FontWeight.w400,
+  );
+  static const bodyTextLarge = TextStyle(
+    fontSize: 20.00,
+    fontWeight: FontWeight.w400,
+  );
+}

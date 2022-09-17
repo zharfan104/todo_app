0d10773e7b8c2547f0d344f30d7b33e5e2f3f302
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-09-17 19:16:31 +0700

    Add ErrorTextBanner

---
+++ shared_libraries/component/lib/banners/error_text_banner.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ErrorTextBanner extends StatelessWidget {
+  const ErrorTextBanner({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String? text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text(text ?? ''));
+  }
+}
--- shared_libraries/component/lib/component.dart
+++ shared_libraries/component/lib/component.dart
@@ -1,3 +1,4 @@
 export 'app_bars/index.dart';
+export 'banners/index.dart';
 export 'dialogs/index.dart';
 export 'loadings/index.dart';

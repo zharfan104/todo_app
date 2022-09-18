f0703ed8eea02a73db5abd1356413497e521f2bc
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-09-18 20:13:22 +0700

    Implement authentication at ApiInterceptors

--- shared_libraries/core/lib/network/dio_handler/api_interceptors.dart
+++ shared_libraries/core/lib/network/dio_handler/api_interceptors.dart
@@ -1,15 +1,15 @@
 import 'dart:convert';
 import 'dart:developer';
 
+import 'package:core/core.dart';
 import 'package:dependencies/dependencies.dart';
+import 'package:router/router.dart';
 
 class ApiInterceptors extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    final requestBody =
-        const JsonEncoder.withIndent('  ').convert(options.data);
-    final queryParameters =
-        const JsonEncoder.withIndent('  ').convert(options.queryParameters);
+    final requestBody = const JsonEncoder.withIndent('  ').convert(options.data);
+    final queryParameters = const JsonEncoder.withIndent('  ').convert(options.queryParameters);
     log(
       'Request URL : ${options.uri}\n'
       '----------------------------------------------------------------------\n'
@@ -21,13 +21,13 @@ class ApiInterceptors extends Interceptor {
       '----------------------------------------------------------------------\n'
       'Method: ${options.method}\n',
     );
+    options.headers = _defaultHeader();
     return handler.next(options);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    final prettyString =
-        const JsonEncoder.withIndent('  ').convert(response.data);
+    final prettyString = const JsonEncoder.withIndent('  ').convert(response.data);
     log(
       'Status Code: ${response.statusCode}\n'
       '——————————————————————————————————————————————————————————————————————\n'
@@ -39,6 +39,19 @@ class ApiInterceptors extends Interceptor {
   @override
   void onError(DioError err, ErrorInterceptorHandler handler) {
     log('Dio Error : $err');
+    if (err.response?.statusCode == 401) {
+      sl<AppRouter>().push(const LoginRoute());
+    }
     return handler.next(err);
+  }
+
+  Map<String, dynamic> _defaultHeader() {
+    AccountManagementService accountManagementService = sl();
+    final token = accountManagementService.getToken();
+
+    Map<String, String> headers = {};
+    headers['Content-Type'] = 'application/json';
+    headers['Authorization'] = 'Bearer $token';
+    return headers;
   }
 }
--- shared_libraries/core/lib/network/dio_handler/dio_handler.dart
+++ shared_libraries/core/lib/network/dio_handler/dio_handler.dart
@@ -15,18 +15,9 @@ class DioHandler {
       connectTimeout: 50000,
       receiveTimeout: 30000,
     );
-    options.headers = _defaultHeader();
     final dio = Dio(options);
     dio.interceptors.add(ApiInterceptors());
 
     return dio;
-  }
-
-  Map<String, dynamic> _defaultHeader() {
-    Map<String, String> headers = {};
-    headers['Content-Type'] = 'application/json';
-    headers['Authorization'] =
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmZjYjM4YWU1MDgwYzAwMTcyMDQ3YTgiLCJpYXQiOjE2NjA3MjgyMDN9.-vrQfE80GNVXCZYT_jha3i2atVM05AayXobTr1u9FCo';
-    return headers;
   }
 }

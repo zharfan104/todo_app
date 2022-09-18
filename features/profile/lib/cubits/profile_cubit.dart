af49bcc009be8381e06318977b53f4d9d0b353bf
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-09-18 12:37:22 +0700

    Add edit and update profile methods in ProfileCubit

--- features/profile/lib/cubits/profile_cubit.dart
+++ features/profile/lib/cubits/profile_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:common/common.dart';
 import 'package:dependencies/dependencies.dart';
 import 'package:profile/cubits/profile_state.dart';
+import 'package:resources/resources.dart';
 
 import '../repositories/profile_repository.dart';
 
@@ -12,12 +13,33 @@ class ProfileCubit extends Cubit<ProfileState> {
 
   ProfileRepository repository;
 
-  void getProfile() async {
+  Future<void> getProfile() async {
     emit(state.copyWith(loadProfileStatus: ViewData.loading()));
     final response = await repository.getProfile();
 
+    _emitProfileResponseToState(response);
+  }
+
+  void onNameChanged(String name) => emit(state.copyWith(name: name));
+  void onEmailChanged(String email) => emit(state.copyWith(email: email));
+  void onAgeChanged(String age) => emit(state.copyWith(age: int.parse(age)));
+
+  void updateProfile() async {
+    emit(state.copyWith(loadProfileStatus: ViewData.loading()));
+
+    final response = await repository.updateProfile(profile: state.toParamUpdateProfileMdl);
+
+    _emitProfileResponseToState(response);
+
+    emit(state.copyWith(loadProfileStatus: ViewData.loaded()));
+  }
+
+  void _emitProfileResponseToState(ResponseGetProfileMdl response) {
     emit(state.copyWith(
-      loadProfileStatus: ViewData.loaded(data: response),
+      name: response.name,
+      email: response.email,
+      age: response.age,
+      loadProfileStatus: ViewData.loaded(),
     ));
   }
 }

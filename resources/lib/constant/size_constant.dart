c88ba2cdedc11dfb220b38ae8682e740853693e4
Zharfan Akbar <zharfan.akbar104@gmail.com>
2022-09-18 20:10:34 +0700

    Add defaultBorderRadius to SizeConstant and fixed spacingSmall value

--- resources/lib/constant/size_constant.dart
+++ resources/lib/constant/size_constant.dart
@@ -1,5 +1,7 @@
 class SizeConstant {
-  static const spacingSmall = 16.0;
+  static const spacingSmall = 8.0;
   static const spacingMedium = 16.0;
   static const spacingBig = 32.0;
+
+  static const defaultBorderRadius = 16.0;
 }
